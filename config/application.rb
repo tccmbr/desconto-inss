e22ac1ac4dea81702e4d2e8e216868c848068835
Ewerton Oliveira <ewerton.tccmbr@gmail.com>
2024-06-05 18:09:52 -0300

    adding translation pt-BR to devise

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module DescontoInss
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :'pt-BR'
   end
 end
