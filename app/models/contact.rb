7440129030bfddb6a5f39508ccbc2b26edbdeb2c
Ewerton Oliveira <ewerton.tccmbr@gmail.com>
2024-06-07 17:10:45 -0300

    adding KINDS constant to list permited kinds

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
+  KINDS = %w[email phone].freeze
+
   belongs_to :owner, polymorphic: true
 
-  as_enum :kind, %i[email phone], map: :string
+  as_enum :kind, KINDS, map: :string
 end
