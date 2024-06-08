0526a381e421578af6ad1f130456a5d34e0b227a
Ewerton Oliveira <ewerton.tccmbr@gmail.com>
2024-06-07 21:15:53 -0300

    adding validations in contribution salary range

--- app/models/inss/contribution_salary_range.rb
+++ app/models/inss/contribution_salary_range.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Inss::ContributionSalaryRange < ApplicationRecord
+  validates :salary_range_number, numericality: { only_integer: true, greater_than: 0 }, uniqueness: true # rubocop:disable Rails/UniqueValidationWithoutIndex
+  validates :start_salary_range, numericality: { greater_than_or_equal_to: 0 }
+  validates :end_salary_range, numericality: { greater_than: 0 }
+  validates :aliquot, numericality: { greater_than: 0 }
 end
